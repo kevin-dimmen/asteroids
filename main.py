0d8f2700aba3fcd884c88675497c2cfaec504192
kevin-dimmen <kevdimmen@gmail.com>
2025-01-20 14:05:49 -0700

    Adding player with movement

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 """
 Asteroids game.
 """
+
 import pygame
 
-from constants import SCREEN_HEIGHT
-from constants import SCREEN_WIDTH 
+from player import Player
 
-from constants import ASTEROID_MIN_RADIUS 
-from constants import ASTEROID_KINDS 
-from constants import ASTEROID_SPAWN_RATE 
-from constants import ASTEROID_MAX_RADIUS 
+from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH
+
+from constants import ASTEROID_MIN_RADIUS
+from constants import ASTEROID_KINDS
+from constants import ASTEROID_SPAWN_RATE
+from constants import ASTEROID_MAX_RADIUS
 from constants import COLOR_BLACK
 
 import os
 
 # fix needed for pygame init
-os.environ['SDL_AUDIODRIVER'] = 'dsp'
+os.environ["SDL_AUDIODRIVER"] = "dsp"
+
+ORIGIN = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
 def main() -> None:
@@ -25,6 +30,16 @@ def main() -> None:
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # group setup
+    updatable  = pygame.sprite.Group()
+    drawable   = pygame.sprite.Group()
+
+    # player setup
+    Player.containers = (updatable, drawable)
+    player = Player(*ORIGIN)
 
     while True:
         for event in pygame.event.get():
@@ -32,9 +47,13 @@ def main() -> None:
                 return
 
         screen.fill(COLOR_BLACK)
-        pygame.display.flip()
+        for x in updatable:
+            x.update(dt)
+        for x in drawable:
+            x.draw(screen)
 
-    print("Game over")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,47 @@
+from circleshape import CircleShape
+from constants import COLOR_WHITE
+from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
+import pygame
+
+from typing import Tuple
+
+class Player(CircleShape):
+    """Player character."""
+
+    def __init__(self, x: int, y: int) -> None:
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.color = COLOR_WHITE
+        self.width = 2
+
+    def triangle(self) -> Tuple[int,int, int]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen) -> None:
+        pygame.draw.polygon(screen, self.color, self.triangle(), self.width)
+
+    def rotate(self, dt: int) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt) -> None:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
